6fdd05765b0770f7ba4876c354c49829d1da285c
mtshah92 <mtshah92@gmail.com>
2022-09-22 13:05:51 +0530

    style updated

--- main.js
+++ main.js
@@ -2,17 +2,27 @@ var translatebtn = document.querySelector(".btn-translate");
 var txtArea = document.querySelector("textarea");
 var outputTxt = document.querySelector(".output-txt");
 
-function clickEventHander() {
-  console.log("clicked!");
-  const inputValue = parseInt(txtArea.value, 10);
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
-  for (var i = 2; i < inputValue; i++) {
-    if (inputValue % i == 0) {
-      outputTxt.innerText = txtArea.innerText + " is not prime.";
-    } else {
-      outputTxt.innerText = txtArea.innerText + " is prime.";
-    }
-  }
+function translatedURL(input) {
+  return serverURL + "?" + "text=" + input;
+}
+
+function errorHandler() {
+  console.log("error occured", error);
+  alert("something went wrong please try after some time");
+}
+
+function clickEventHander() {
+  var inputTxt = txtArea.value;
+
+  fetch(translatedURL(inputTxt))
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedTxt = json.contents.translated;
+      outputTxt.innerText = translatedTxt;
+    })
+    .catch(errorHandler);
 }
 
 translatebtn.addEventListener("click", clickEventHander);
